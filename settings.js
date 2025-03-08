613c2ebe36c1df46f6adb68d51e6548cc8bc88f6
alvinzcho <apriliayunianjani@gmail.com>
2025-03-09 00:36:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: '''๋͜͡➣┼ 𝛭𝑅 𝐷𝛩𝑅𝛯𝛭𝛩𝛮 ✞︎ ⃟🇵🇰❤️🔥', // Your name
-  ownerNumber: '923186201007', //Your number
+  botOwner: '''๋͜͡➣┼ 𝛭𝑅 𝐷𝛩𝑅𝛯𝛭𝛩𝛮 ✞︎ ⃟🇵🇰❤️🔥', // alv7z
+  ownerNumber: '923186201007', //6287856141568
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
